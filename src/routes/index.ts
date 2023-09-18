38238d1e43288cb7eb65923c26ae1dc5157f42e5
Roseane Gama <rocg@cin.ufpe.br>
2023-09-18 16:52:26 -0300

    refactor

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,15 @@
 import { NextFunction, Request, Response, Router } from "express";
 import routes from "./MailRoutes";
 import UserRouter from "./UserRoutes";
-import { login, recoverUserInfo } from "src/authentication";
+import AuthenticationRouter from "./AuthenticationRouter";
 
 const router = Router();
 
 router.use("/user", UserRouter);
 router.use("/email", routes);
+router.use("/auth", AuthenticationRouter);
 router.route("/").get((req, res) => {
   res.send("Made with 💚 and &lt; &#x0002F; &gt; by CITi");
 });
-
-router.route("/login").post(login);
-router.route("/recoverUserInfo").get(recoverUserInfo);
 
 export default router;
